46a98198c9f5bfe07ceb9badf0e1d615482e84e2
Ian Snyder <idsnyder136@gmail.com>
2023-08-31 15:28:08 -0400

    added Flan-T5 support

--- ScholarlyRecommender/Newsletter/feed.py
+++ ScholarlyRecommender/Newsletter/feed.py
@@ -27,174 +27,37 @@ def extract_author_names(author_string):
 # Pipeline()
 
 
-# df = clean_feed()
-
-
-def build_html_feed(
-    df: pd.DataFrame, to_path: str = "ScholarlyRecommender/Newsletter/html/Feed.html"
-):
-    # df = pd.read_csv("Repository/CleanFeed.csv")
-
-    # Initialize an empty string to store the HTML content
-    html_content = """<!DOCTYPE html>
-    <html>
-    <head>
-    <meta charset="utf-8"/><meta content="width=device-width, initial-scale=1.0" name="viewport"/><link href="https://fonts.googleapis.com/css2?family=Open+Sans&amp;display=swap" rel="stylesheet"/>
-
-    <title>Weekly Newsletter</title>
-    <style>
-    .title-main{
-    font-family: "Helvetica Neue", sans-serif;
-    font-size: 28px;
-    letter-spacing: 0.05em;
-    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
-    color: #2C3E50;
-    margin-bottom: 10px;
-}
-p {
-    font-family: "Georgia", serif;
-    font-size: 18px;
-    line-height: 1.6;
-    text-align: justify;
-    color: #333;
-    margin-bottom: 20px;
-}
-/* General styles for each feed item */
-.feed-item {
-    border: 1px solid #ccc;
-    padding: 15px;
-    margin: 15px;
-    border-radius: 8px;
-}
-
-/* Style for the title */
-.title {
-    font-size: 24px;
-    font-weight: bold;
-    margin-bottom: 5px;
-}
-.author {
-    font-size: 14px;
-    font-weight: bold;
-    margin-bottom: 10px;
-}
-/* Style for the metadata section */
-.metadata {
-    font-size: 14px;
-    
-    margin-bottom: 10px;
-}
-
-/* Style for the abstract */
-.abstract {
-    font-size: 16px;
-    margin-bottom: 10px;
-}
-
-/* Style for the read more link */
-a {
-    display: inline-block;
-    background-color: #007BFF;
-    color: white;
-    padding: 8px 16px;
-    border-radius: 4px;
-    text-decoration: none;
-}
-    </style><style>
-/* Responsive Design */
-@media only screen and (max-width: 600px) {
-    body {
-        font-size: 16px;
-    }
-}
-
-/* Custom Fonts */
-body {
-    font-family: 'Open Sans', sans-serif;
-}
-
-/* Improved Color Scheme */
-.title-main {
-    color: #34495e;
-}
-
-p {
-    color: #2c3e50;
-}
-</style>
-        
-    </head>
-    <body>
-    <h2 class="title-main" style='font-family: "Open Sans", sans-serif; color: #34495e;'>Decoding the Future of AI: Ethical Dilemmas, Embedding Logic, and Emotion Analytics in Conversational Tech—A Curated Selection of Cutting-Edge Research</h2>
-<p style='font-family: "Open Sans", sans-serif; color: #2c3e50;'>
-    Dear Reader,
-    </p>
-<p style='font-family: "Open Sans", sans-serif; color: #2c3e50;'>
-        This edition aims to immerse you in the nuanced fabric of Large Language Models, spanning topics from ethical concerns in moderation and gender biases to highly technical advancements like embedding logic programming into Python's deep-learning ecosystem. Expect in-depth analyses and pioneering solutions that push the boundaries of conventional wisdom, handpicked to empower your specialized interest in these paradigms.
-</p>"""
-
-    # HTML template for each feed item
-    html_template = """
-    <div class="feed-item">
-    <h2 class="title">{title}</h2>
-    <h4 class="author">{author}</h4>
-    <div class="metadata">
-        <span class="id">{id}</span> | 
-        <span class="category">{category}</span> | 
-        <span class="published">{published}</span>
-    </div>
-    <div class="abstract">
-        {abstract}
-    </div>
-    <a href="{url}" target="_blank">Read More</a>
-    </div>
-    """
-
-    # Iterate through the DataFrame and fill in the HTML template
-    for index, row in df.iterrows():
-        item_html = html_template.format(
-            title=row["Title"],
-            author=row["Author"],
-            id=row["Id"],
-            category=row["Category"],
-            published=row["Published"],
-            abstract=row["Abstract"],
-            url=row["URL"],
-        )
-        html_content += item_html
-    html_content += """ </body>
-    </html>"""
-    # Save the generated HTML to a file for demonstration
-    html_file_path = to_path
-    with open(html_file_path, "w") as f:
-        f.write(html_content)
-
-    html_file_path
-
-
 def build_email(
     df: pd.DataFrame, to_path: str = "ScholarlyRecommender/Newsletter/html/Feed.html"
 ):
-    # df = pd.read_csv("Repository/CleanFeed.csv")
+    flanT5_out = {
+        "headline": "Decoding the Future of AI: Ethical Dilemmas, Embedding Logic, and Emotion Analytics in Conversational Tech—A Curated Selection of Cutting-Edge Research",
+        "intro": "This edition aims to immerse you in the nuanced fabric of Large Language Models, spanning topics from ethical concerns in moderation and gender biases to highly technical advancements like embedding logic programming into Python's deep-learning ecosystem. Expect in-depth analyses and pioneering solutions that push the boundaries of conventional wisdom, handpicked to empower your specialized interest in these paradigms.",
+    }
 
-    # Initialize an empty string to store the HTML content
     html_content = """<!DOCTYPE html>
     <html>
     <body>
-    <h2 class="title-main" style='font-family: "Open Sans", sans-serif; color: #34495e;font-family: Arial, sans-serif;
+    """
+    body_template = """
+<h2 class="title-main" style='font-family: "Open Sans", sans-serif; color: #34495e;font-family: Arial, sans-serif;
     font-size: 28px;
     letter-spacing: 0.05em;
     
     color: #2C3E50;
-    margin-bottom: 10px;'>Decoding the Future of AI: Ethical Dilemmas, Embedding Logic, and Emotion Analytics in Conversational Tech—A Curated Selection of Cutting-Edge Research</h2>
-<p style='font-family: "Open Sans", sans-serif; color: #2c3e50;'>
+    margin-bottom: 10px;'>{headline}</h2>
+<p style='font-family: "Open Sans", sans-serif; color: #2c3e50; font-size: 18px;  margin-bottom: 20px; line-height: 1.6;'>
     Dear Reader,
     </p>
-<p style='font-family: "Open Sans", sans-serif; color: #2c3e50;'>
-        This edition aims to immerse you in the nuanced fabric of Large Language Models, spanning topics from ethical concerns in moderation and gender biases to highly technical advancements like embedding logic programming into Python's deep-learning ecosystem. Expect in-depth analyses and pioneering solutions that push the boundaries of conventional wisdom, handpicked to empower your specialized interest in these paradigms.
+<p style='font-family: "Open Sans", sans-serif; color: #2c3e50; font-size: 18px;  margin-bottom: 20px; line-height: 1.6;'>
+        {intro}
 </p>
-   """
-
+    """
+    body_html = body_template.format(
+        headline=flanT5_out["headline"],
+        intro=flanT5_out["intro"],
+    )
+    html_content += body_html
     # HTML template for each feed item
     html_template = """
     <div class="feed-item" style="border: 1px solid #ccc;
@@ -203,7 +66,7 @@ def build_email(
     border-radius: 8px;">
     <h2 class="title" style=" font-size: 24px;
     font-weight: bold;
-    margin-bottom: 5px;">{title}</h2>
+    margin-bottom: 5px; color: #000000;">{title}</h2>
     <h4 class="author" style="font-size: 14px;
     font-weight: bold;
     margin-bottom: 10px; color:#000000;">{author}</h4>
--- testing.py
+++ testing.py
@@ -2,6 +2,8 @@ import ScholarlyRecommender as sr
 import pandas as pd
 import numpy as np
 
+# from test_mail import send_email
+
 
 def Pipeline():
     # Scrape
@@ -14,11 +16,16 @@ def Pipeline():
 
     sr.get_feed(
         data=r,
-        to_path="ScholarlyRecommender/Newsletter/html/TestUserFeed.html",
+        to_path="ScholarlyRecommender/Newsletter/html/NewTestFeed.html",
+    )
+    # send_email(path="ScholarlyRecommender/Newsletter/html/TestFeed.html")
+
+
+def short_Pipeline():
+    sr.get_feed(
+        data="ScholarlyRecommender/Repository/Recommendations.csv",
+        to_path="ScholarlyRecommender/Newsletter/html/NewTestFeed.html",
     )
 
 
-sr.get_feed(
-    data="ScholarlyRecommender/Repository/Recommendations.csv",
-    to_path="ScholarlyRecommender/Newsletter/html/NewTestFeed.html",
-)
+short_Pipeline()
