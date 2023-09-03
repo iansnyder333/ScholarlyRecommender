42d4e1245ead7e72e9e82a9a735a0b389d2ff217
Ian Snyder <idsnyder136@gmail.com>
2023-09-03 15:47:18 -0400

    Added email, ready for public test

--- ScholarlyRecommender/Newsletter/mail.py
+++ ScholarlyRecommender/Newsletter/mail.py
@@ -2,28 +2,20 @@ import smtplib
 from email.message import EmailMessage
 
 
-def send_email(
-    path: str = "ScholarlyRecommender/Newsletter/html/NewTestFeed.html",
-):
-    EMAIL_ADDRESS = "scholarlyrecommender@gmail.com"
+def send_email(**kwargs):
+    EMAIL_ADDRESS = kwargs["email"]
 
-    EMAIL_PASSWORD = input("*")
+    EMAIL_PASSWORD = kwargs["password"]
 
-    SUBSCRIBERS = [
-        "scholarlyrecommender@gmail.com",
-    ]
+    SUBSCRIBERS = kwargs["subscribers"]
 
     msg = EmailMessage()
     msg["Subject"] = "Your Scholarly Recommender Weekly Newsletter"
     msg["From"] = EMAIL_ADDRESS
     msg["To"] = SUBSCRIBERS
 
-    with open(path, "r") as f:
-        html_string = f.read()
+    html_string = kwargs["content"]
     msg.set_content(html_string, subtype="html")
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         smtp.send_message(msg)
-
-
-# send_email(path="ScholarlyRecommender/Newsletter/html/NewTestFeed.html")
