c8c2d71e3fed507298804883d4603e26f737bdc0
Ian Snyder <idsnyder136@gmail.com>
2023-09-04 09:54:25 -0400

    Significant UX Improvements
    UI Overhaul, plus email should be working now.

--- webapp.py
+++ webapp.py
@@ -71,9 +71,7 @@ def send_email(**kwargs):
         print(f"An error occurred: {e}")
 
 
-def generate_feed_pipeline(
-    query: list, n: int, days: int, to_email: bool, user_email: str
-):
+def generate_feed_pipeline(query: list, n: int, days: int, to_email: bool):
     with st.status("Working...", expanded=True) as status:
         status.update(label="Searching for papers...", state="running", expanded=True)
 
@@ -101,14 +99,7 @@ def generate_feed_pipeline(
             web=True,
         )
         status.update(label="Feed Generated", state="complete", expanded=False)
-    if not to_email or user_email is None:
-        components.html(source_code, height=1000, scrolling=True)
-    elif to_email and user_email is not None:
-        send_email(
-            subscribers=[user_email],
-            content=source_code,
-        )
-        st.success("Email sent successfully!")
+        return source_code
 
 
 def fetch_papers(num_papers: int = 10) -> pd.DataFrame:
@@ -163,7 +154,9 @@ def calibrate_rec_sys(num_papers: int = 10):
         old_config = get_sc_config()
         old_config["labels"] = st.session_state.papers_df
         update_sc_config(old_config)
-        st.success("Labels saved.")
+        st.success(
+            "Labels saved. Get recommendations is now configured to your interests."
+        )
         # Increment to prevent re-running this block
         st.session_state.current_index += 1
 
@@ -173,9 +166,10 @@ def calibrate_rec_sys(num_papers: int = 10):
 
 if "sys_config" not in st.session_state:
     st.session_state.sys_config = load_sc_config()
+
 # Theme Configuration
 st.set_page_config(
-    page_title="Scholarly Recommender",
+    page_title="Scholarly Recommender API",
     page_icon="images/logo.png",
     layout="wide",
     initial_sidebar_state="expanded",
@@ -200,20 +194,39 @@ navigation = st.sidebar.radio(
 
 # Home Page
 if navigation == "Get Recommendations":
-    st.title("Scholarly Recommender")
+    st.title("Scholarly Recommender Cloud API")
+    st.markdown(
+        """
+        ## Welcome to the Scholarly Recommender API
+
+        This platform is designed to offer you highly tailored scholarly recommendations. 
+        Whether you're a researcher, academic, or just someone interested in scientific literature, 
+        this service is built to cater to your specific needs.
+
+        ### To Get Started
+
+        - **Configure**: The first step is to configure the system to your interests. This can be done by navigating to the configure page. If you skip this step, the system will use a default configuration tailored to the interests of the developer.
+        - **Get Recommendations**: Once you've configured the system, you can get recommendations by pressing the 'generate recommendations' button at the bottom of the page.
+        - **Categories & Sub-Categories**: You can customize your interests by selecting from a wide range of categories and sub-categories, by default, the system will search based on your configured interests.
+        - **Recommendation Count**: Choose how many recommendations you want to receive.
+        - **Time Range**: Decide the time frame for the articles you're interested in.
+        
+        """,
+        unsafe_allow_html=True,
+    )
     # User input section
     st.subheader("Customize your recommendations")
 
     # Collecting user details
 
     categories = st.multiselect(
-        "What Interests You? (leave blank for default configuration)",
+        "Would you like to search for any specific categories? (leave blank to use your configured interests)",
         search_categories.keys(),
     )
     selected_sub_categories = {}
     for selected in categories:
         selected_sub_categories[selected] = st.multiselect(
-            f"Select sub-categories under {selected} (Optional)",
+            f"Select sub-categories under {selected} (leave blank for all)",
             search_categories[selected],
         )
 
@@ -229,96 +242,173 @@ if navigation == "Get Recommendations":
         max_value=30,
         value=7,
     )
+
     to_email = st.checkbox("Email Recommendations?")
+
     if to_email:
-        # user_email = st.text_input("your email address")
-        st.write("This feature is currently under development")
-        user_email = st.text_input("your email address")
-        st.write(
-            "Disclaimer: Scholarly Recommender will only send you an email with your recommendations"
-        )
-        st.write(
-            "We will not save your email and it will never be used for any other purpose"
-        )
+        with st.form("email_form"):
+            st.write(
+                "This feature is currently under development, please report any issues you encounter."
+            )
+            user_email = st.text_input(
+                "your email address", placeholder="example@email.com", value=""
+            )
+            st.markdown(
+                """Disclaimer: Scholarly Recommender will only send you an email with your recommendations
+            and will not use your email address for any other purpose.""",
+                unsafe_allow_html=True,
+            )
+
+            submit_button = st.form_submit_button(label="Confirm")
+            if submit_button:
+                if validate_email(user_email):
+                    st.success("Email address confirmed")
+
+                else:
+                    st.error("Please enter a valid email address")
+
     else:
-        user_email = None
+        user_email = ""
     # Call to Action
     if st.button("Generate Recommendations", type="primary"):
         query = build_query(selected_sub_categories)
-        generate_feed_pipeline(query, n, days, to_email, user_email)
+        source_code = generate_feed_pipeline(query, n, days, to_email)
+        if to_email:
+            send_email(
+                subscribers=[user_email],
+                content=source_code,
+            )
+            st.success("Email sent successfully!")
+        components.html(source_code, height=1000, scrolling=True)
 
 
 elif navigation == "Configure":
-    st.title("Scholarly Recommender Configuration")
-    # User input section
-    st.subheader("Welcome to the Scholarly Recommender System Calibration Tool \n")
-    st.write(
-        "This tool will help you calibrate the recommender system to your interests \n"
-    )
-    st.write(
-        "Below are the various configuration steps, it is advised to do them in order \n"
-    )
-    with st.expander("Calibrate Interests"):
-        st.write(
-            "Please answer the following questions to help us get to know you better \n"
-        )
-        st.write("Select the categories that interest you the most: \n")
-        # Collecting user details
+    st.title("Scholarly Recommender Configuration API")
+    st.markdown(
+        """
+        ## Welcome to the Scholarly Recommender System Calibration Tool
 
-        categories = st.multiselect(
-            "What Interests You? (select at least one)",
-            search_categories.keys(),
+        This tool will help you calibrate the recommender system to your interests!
+        Below are the various configuration steps, it is advised to do them in order.
+        Once a step is completed, the changes will be applied automatically, regardless of whether you continue to the next step or not.
+                """
+    )
+    # User input section
+    st.markdown(
+        """
+        ### Configure your interests
+            
+        This section will help you configure the system to your interests.
+        This insures the system will use this to only scrape papers relevant to you.
+        Follow the steps below to get started.
+                """
+    )
+
+    categories = st.multiselect(
+        "Select the categories that interest you the most, at least one is required:",
+        search_categories.keys(),
+    )
+    selected_sub_categories = {}
+    for selected in categories:
+        selected_sub_categories[selected] = st.multiselect(
+            f"Select sub-categories under {selected} (Optional, leave blank for all)",
+            search_categories[selected],
         )
-        selected_sub_categories = {}
-        for selected in categories:
-            selected_sub_categories[selected] = st.multiselect(
-                f"Select sub-categories under {selected} (Optional)",
-                search_categories[selected],
-            )
-        if st.button("Done"):
-            with st.spinner("Configuring..."):
-                query = build_query(selected_sub_categories)
-                # prevent empty queries
-                assert len(query) > 0, "Please select at least one interest."
-                configuration = get_sc_config()
-                configuration["queries"] = query
-                update_sc_config(configuration)
+    if st.button("Done", type="primary"):
+        with st.spinner("Configuring..."):
+            query = build_query(selected_sub_categories)
+            # prevent empty queries
+            assert len(query) > 0, "Please select at least one interest."
+            configuration = get_sc_config()
+            configuration["queries"] = query
+            update_sc_config(configuration)
 
             st.success("Preferences updated successfully!")
     # Initialize a session state variable for calibration status if it doesn't exist
     if "calibration_started" not in st.session_state:
         st.session_state.calibration_started = False
 
-    with st.expander("Calibrate Rec Sys"):
-        st.write(
-            "Please rate the following papers on a scale of 1 to 10, 1 being the least relevant and 10 being the most relevant \n"
-        )
-        # st.write("You can also skip a paper if you don't want to rate it \n") (TODO: Implement this feature)
-        st.write("You will be shown 10 papers to rate \n")
-        st.write("Click on the button below to get started \n")
-        st.write(
-            "Note: Currently, if you want to start over or repeat this process, you must refresh the page \n"
-        )  # TODO: Fix this
-        if st.button("Start Calibration"):
-            st.session_state.calibration_started = True
+    st.markdown(
+        """
+        ### Calibrate the Recommender System
+        
+        This section will help you calibrate the recommender system based on your interests.
+        This will help the system learn your preferences and will significantly improve recommendations.
+        This process will show you snippets of 10 papers and ask you to rate them on a scale of 1 to 10 (1 being the least relevant and 10 being the most relevant).
+        Many improvements are planned for this process, including the ability to skip papers, change sample size, and dynamically update the system based on your feedback from the generated feed.
 
-        if st.session_state.calibration_started:
-            with st.spinner("Preparing Calibration..."):
-                calibrate_rec_sys(num_papers=10)
+        Click on the button below to get started. 
+
+        **Note** : Currently, if you want to start over or repeat this process, you must refresh the page
+        """
+    )
+
+    if st.button("Start Calibration", type="primary"):
+        st.session_state.calibration_started = True
+
+    if st.session_state.calibration_started:
+        with st.spinner("Preparing Calibration..."):
+            calibrate_rec_sys(num_papers=10)
 
 # About Page
 elif navigation == "About":
-    st.title("About")
-    st.write(
-        "As an upcoming data scientist with a strong passion for deep learning, I am always looking for new technologies and methodologies. Naturally, I spend a considerable amount of time researching and reading new publications to accomplish this. However, over 14,000 academic papers are published every day on average, making it extremely tedious to continuously source papers relevant to my interests. My primary motivation for creating ScholarlyRecommender is to address this, creating a fully automated and personalized system that prepares a feed of academic papers relevant to me. This feed is prepared on demand, through a completely abstracted streamlit web interface, or sent directly to my email on a timed basis. This project was designed to be scalable and adaptable, and can be very easily adapted not only to your own interests, but become a fully automated, self improving newsletter. Details on how to use this system, the methods used for retrieval and ranking, along with future plans and features planned or in development currently are listed below."
+    st.markdown(
+        """
+        ## Welcome to the Scholarly Recommender Cloud API
+
+        This platform is designed to offer you highly tailored scholarly recommendations. 
+        Whether you're a researcher, academic, or just someone interested in scientific literature, 
+        this service is built to cater to your specific needs.
+
+        ### How It Works
+        
+        - **Configure**: The first step is to configure the system to your interests. This can be done by navigating to the configure page.
+        - **Get Recommendations**: Once you've configured the system, you can generate recommendations by navigating to the get recommendations page.
+        - **Categories & Sub-Categories**: You can customize your interests by selecting from a wide range of categories and sub-categories, by default, the system will search based on your configured interests.
+        - **Recommendation Count**: Choose how many recommendations you want to receive.
+        - **Time Range**: Decide the time frame for the articles you're interested in.
+
+        ### What Makes Us Different
+
+        - **Personalized**: Recommendations are fine-tuned to match your unique academic interests.
+        - **Up-to-Date**: The platform provides options to focus on the most recent articles.
+        - **Quality Assured**: We prioritize recommendations from reputable sources and peer-reviewed journals.
+
+        ### Mission Statement
+
+        As an upcoming data scientist with a strong passion for deep learning, I am always looking for new technologies and methodologies. Naturally, I spend a considerable amount of time researching and reading new publications to accomplish this. However, over 14,000 academic papers are published every day on average, making it extremely tedious to continuously source papers relevant to my interests. My primary motivation for creating ScholarlyRecommender is to address this, creating a fully automated and personalized system that prepares a feed of academic papers relevant to me. This feed is prepared on demand, through a completely abstracted streamlit web interface, or sent directly to my email on a timed basis. This project was designed to be scalable and adaptable, and can be very easily adapted not only to your own interests, but become a fully automated, self improving newsletter. Details on how to use this system, the methods used for retrieval and ranking, along with future plans and features planned or in development currently are listed below."
+
+   
+        """
     )
 
 # Contact Page
 elif navigation == "Contact":
-    st.title("Contact")
-    st.write("Email: Project Email - scholarlyrecommender@gmail.com")
-    st.write("My Website: https://iansnyder333.github.io/frontend/")
-    st.write("My LinkedIn: www.linkedin.com/in/ian-snyder-aa1600182/")
+    st.markdown(
+        """
+        <h1 align="center">Contact Information</h1>
+
+        ## Project Support
+
+        Please report any issues by creating an issue on the GitHub repository, or by sending an email to the project email directly.
+
+        - **Github Issue**: https://github.com/iansnyder333/ScholarlyRecommender/issues
+        - **Project Email**: scholarlyrecommender@gmail.com
+
+        ## Developer Contact Information
+
+        If you have any questions or concerns, please feel free to reach out to me directly.
+        I recently graduated college and am the sole developer of this project, so I would love any constructive feedback you have to offer to help me improve as a developer.
+
+        - **Ian Snyder**: [@iansnydes](https://twitter.com/iansnydes) - idsnyder136@gmail.com 
+        - **Website and Portfolio**: [iansnyder333.github.io/frontend/](https://iansnyder333.github.io/frontend/)
+        - **LinkedIn**: [www.linkedin.com/in/ian-snyder-aa1600182/](https://www.linkedin.com/in/ian-snyder-aa1600182/)
+        
+
+                """,
+        unsafe_allow_html=True,
+    )
+
 
 # Footer
 st.markdown(
