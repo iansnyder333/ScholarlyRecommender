e63789f5c6bb157a9697a7e9029bb6074cfd7644
Ian Snyder <idsnyder136@gmail.com>
2023-09-22 09:58:00 -0400

    Update mail.py
    Added instructions for email on local clone.

--- ScholarlyRecommender/Newsletter/mail.py
+++ ScholarlyRecommender/Newsletter/mail.py
@@ -9,3 +9,34 @@ def validate_email(email):
     if re.match(regex, email):
         return True
     return False
+
+
+"""
+Example of how to configure the email server if you have installed ScholarlyRecommender locally:
+Replace the constants with your own email address, password, port number and subscribers (delivery address).
+Note that you need to enable less secure apps in your google account settings, otherwise this will not work.
+Once you have your own constants working, you can delete the input() calls and replace them with your own constants.
+To send an email, simply call send_email(content=html_string) from the Newsletter module. 
+html_string is the return value from the get_feed() function in feed.py.
+"""
+
+
+def send_email(**kwargs):
+    EMAIL_ADDRESS = input("Enter your email address: ")
+    EMAIL_PASSWORD = input("Enter your email password: ")
+    SUBSCRIBERS = input(
+        "Enter your subscribers email addresses (separated by commas): "
+    )
+    PORT = input("Enter your port number: (465 for gmail)")
+
+    msg = EmailMessage()
+    msg["Subject"] = "Your Scholarly Recommender Newsletter"
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = SUBSCRIBERS
+
+    html_string = kwargs["content"]
+
+    msg.set_content(html_string, subtype="html")
+    with smtplib.SMTP_SSL("smtp.gmail.com", PORT) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
