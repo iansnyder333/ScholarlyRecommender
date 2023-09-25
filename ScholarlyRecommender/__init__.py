4447c304b8de5ebbfc10c3d690fb542cb89dcd86
Ian Snyder <idsnyder136@gmail.com>
2023-09-25 08:39:40 -0400

    improved testing

--- ScholarlyRecommender/__init__.py
+++ ScholarlyRecommender/__init__.py
@@ -1,6 +1,5 @@
 from .Scraper.Arxiv import source_candidates
-from .Recommender.rec_sys import get_recommendations
-from .Recommender.rec_sys import evaluate
+from .Recommender.rec_sys import get_recommendations, evaluate
 from .Newsletter.feed import get_feed
 
 from .config import get_config, update_config
