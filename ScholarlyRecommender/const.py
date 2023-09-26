1d1c0a0c3f5073efb25b5d575ac5bbe06c42e834
Ian Snyder <58576523+iansnyder333@users.noreply.github.com>
2023-09-26 11:25:59 +0000

    added docstring

--- ScholarlyRecommender/const.py
+++ ScholarlyRecommender/const.py
@@ -1,7 +1,13 @@
 from copy import deepcopy
 
 
-def BASE_REPO(): return deepcopy(
+def BASE_REPO():
+    """
+    initialize a new base repo as a deep copy
+    @param: None
+    @return: a dictionary (str:list)
+    """
+    return deepcopy(
     {
         "Id": [],
         "Category": [],
